d073a5a3059412b34d9e5dc1e005da35fe2ea0fa
davydovanika <nikadavydova@gmail.com>
2022-05-02 15:36:02 -0400

    update2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 import java.util.Date;
+import java.util.Formatter;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println(new Date());
+        System.out.println("Good day");
     }
 }
